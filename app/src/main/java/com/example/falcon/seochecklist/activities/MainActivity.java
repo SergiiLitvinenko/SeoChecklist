ecdb3c9901ba3c2dccdfa43dc99e42ca7a086e85
Sergii Litvinenko <litvinenko.sergii@gmail.com>
2016-10-09 21:04:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/falcon/seochecklist/activities/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.falcon.seochecklist.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.falcon.seochecklist.R;
+import com.example.falcon.seochecklist.fragments.MenuFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MenuFragment.IOnMyMenuButtonClick {
+    FrameLayout fragmentContainer;
+    FragmentTransaction fTrans;
+    Fragment menuFragment, checklistFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentContainer = (FrameLayout) findViewById(R.id.flFragmentContainer);
+
+        menuFragment = new MenuFragment();
+        fTrans = getSupportFragmentManager().beginTransaction();
+        fTrans.replace(R.id.flFragmentContainer, menuFragment);
+        fTrans.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+
+        }
+    }
+
+    @Override
+    public void menuButtonClick() {
+        Toast.makeText(this, "Yay! Button clicked!", Toast.LENGTH_SHORT).show();
+        Log.v("myLog", "menuButtonClick");
+    }
+}
---
+++ app/src/main/java/com/example/falcon/seochecklist/fragments/ChecklistFragment.java
@@ -0,0 +1,10 @@
+package com.example.falcon.seochecklist.fragments;
+
+import android.support.v4.app.Fragment;
+
+
+public class ChecklistFragment extends Fragment {
+
+
+
+}
